583c6551ec2a14e1b74dbd82c89c3816aadb8f1b
Varun Sai <vsai99894@gmail.com>
2025-06-15 12:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const products = [
     type: "Car", 
     price: 35000, 
     desc: "High-performance sports car.", 
-    image: "https://cdn.pixabay.com/photo/2012/05/29/00/43/car-49278_640.jpg" 
+    image: "https://cdn.pixabay.com/photo/2015/01/19/13/51/car-604019_640.jpg" 
   },
   { 
     id: 2, 
@@ -13,7 +13,7 @@ const products = [
     type: "Car", 
     price: 28000, 
     desc: "All-terrain SUV ready for adventure.", 
-    image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/automobile-1868726_640.jpg" 
+    image: "https://cdn.pixabay.com/photo/2015/03/26/09/39/jeep-690548_640.jpg" 
   },
   { 
     id: 3, 
@@ -21,7 +21,7 @@ const products = [
     type: "Bike", 
     price: 12000, 
     desc: "Stylish two-wheeler for city rides.", 
-    image: "https://cdn.pixabay.com/photo/2017/03/27/14/56/motorcycle-2178726_640.jpg" 
+    image: "https://cdn.pixabay.com/photo/2016/07/07/16/46/motorcycle-1503800_640.jpg" 
   },
   { 
     id: 4, 
@@ -32,5 +32,3 @@ const products = [
     image: "https://cdn.pixabay.com/photo/2014/10/22/17/22/motorcycle-498244_640.jpg" 
   }
 ];
-
-
